64ca35944b0a56623994f0ac42be44c8e71ed49c
Ali Jundi <@ali.d5>
2022-06-04 18:43:38 +0300

    ttt

--- src/user/auth/auth.strategy.ts
+++ src/user/auth/auth.strategy.ts
@@ -10,13 +10,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   @Inject(AuthHelper)
   private readonly helper: AuthHelper;
 
-  // constructor(@Inject(ConfigService) config: ConfigService) {
-  //   super({
-  //     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  //     secretOrKey: config.get('JWT_KEY'),
-  //     ignoreExpiration: true,
-  //   });
-  // }
+  constructor(@Inject(ConfigService) config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_KEY'),
+      ignoreExpiration: true,
+    });
+  }
 
   private validate(payload: string): Promise<User | never> {
     return this.helper.validateUser(payload);
